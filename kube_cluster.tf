8078171cdb11fed05247f93f9a7a297f01496221
Nikola Orlov <norlov@happiestbaby.com>
2024-06-24 17:41:35 +0200

    hardcode AMI to avoid changes

--- kube_cluster.tf
+++ kube_cluster.tf
@@ -14,11 +14,10 @@ locals {
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240514"]
   }
 
   filter {
